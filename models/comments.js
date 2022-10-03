b631e486cb22d3507042fcda8315f4841e8bd58c
CNRBanks <connortamminen@gmail.com>
2022-10-03 10:38:14 -0500

    Update comments.js
    started comments model

---
+++ models/comments.js
@@ -0,0 +1,36 @@
+const sequelize = require('sequelize');
+const sequelizeConnect = require('../config/sequelizeConnect');
+
+
+const Comments = sequelizeConnect.define('comment', {
+    id: {
+        type: sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+    },
+
+    content: {
+        type: sequelize.TEXT,
+        allowNull: false
+    },
+
+    user_id: {
+        type: sequelize.INTEGER,
+        allowNull: false,
+        reference: {
+            model: "Users",
+            key: 'id'
+        }
+    }
+
+
+}, {
+    sequelize: sequelizeConnect,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: 'comments',
+    underscored: true
+});
+
+module.exports = Comments
